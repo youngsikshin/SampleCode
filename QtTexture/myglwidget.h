92ea2923f2f62608c21adc736ebf8dbeaa67a42e
youngsikshin <bluevow@gmail.com>
2016-12-06 01:26:08 +0900

    add Coloring and Texturing example

---
+++ QtTexture/myglwidget.h
@@ -0,0 +1,47 @@
+#ifndef MYGLWIDGET_H
+#define MYGLWIDGET_H
+
+#include <QGLWidget>
+#include <QVector>
+#include <QGLShaderProgram>
+#include <QMouseEvent>
+#include <QPoint>
+
+class MyGLWidget : public QGLWidget
+{
+    Q_OBJECT
+public:
+    explicit MyGLWidget(QWidget *parent = 0);
+    ~MyGLWidget();
+
+signals:
+
+public slots:
+
+protected:
+    void initializeGL();
+    void paintGL();
+    void resizeGL(int width, int height);
+
+    QSize minimumSizeHint() const;
+    QSize sizeHint() const;
+
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void wheelEvent(QWheelEvent *event);
+
+private:
+    QMatrix4x4 pMatrix;
+    QGLShaderProgram shaderProgram;
+    QVector<QVector3D> vertices;
+    QVector<QVector3D> colors;
+    QVector<QVector2D> textureCoordinates;
+    GLuint texture;
+
+    double alpha;
+    double beta;
+    double distance;
+    QPoint lastMousePosition;
+};
+
+#endif // MYGLWIDGET_H
